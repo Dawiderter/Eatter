cbbfa33f67ddb0ee3997b7b63578dd04d40ff337
Dawiderter <dwalczak456@gmail.com>
2023-01-18 19:38:14 +0100

    Toying around with frontend

---
+++ eatter_frontend/src/lib/Feed.svelte
@@ -0,0 +1,22 @@
+<script>
+    import Post from "./Post.svelte";
+    let posts = [
+        "post1",
+        "post2",
+        "post3"
+    ]
+</script>
+
+<section>
+    {#each posts as post}
+    <Post content = {post}/>
+    {/each}
+</section>
+
+<style>
+    section {
+        width: auto;
+    }
+</style>
+
+    
---
+++ eatter_frontend/src/lib/Post.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let content
+</script>
+
+<article>
+    {content}
+</article>
+
+<style>
+    article {
+        background-color: gray;
+        margin: 10px;
+        padding: 10px;
+    }
+</style>
