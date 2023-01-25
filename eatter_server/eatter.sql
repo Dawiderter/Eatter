9687638343003dd785b21c101e1b7d9325433594
Dawiderter <dwalczak456@gmail.com>
2023-01-25 17:59:22 +0100

    Changed feed item

--- eatter_server/eatter.sql
+++ eatter_server/eatter.sql
@@ -97,8 +97,8 @@ CREATE TABLE followers (
 	FOREIGN KEY (followed) REFERENCES users(id)
 );
 
-CREATE VIEW feed AS SELECT r.id AS r_id, r.body AS r_body, r.created_at AS r_created_at, r.score AS r_score, r.author_id AS r_author_id, m.id AS m_id, m.name AS m_name, l.id AS l_id, l.name AS l_name 
-	FROM reviews r JOIN meals m ON r.meal_id = m.id JOIN locals l ON m.local_id = l.id;
+CREATE VIEW feed AS SELECT r.id AS r_id, r.body AS r_body, r.created_at AS r_created_at, r.score AS r_score, u.id AS u_id, u.nick AS u_nick, m.id AS m_id, m.name AS m_name, l.id AS l_id, l.name AS l_name 
+	FROM reviews r JOIN meals m ON r.meal_id = m.id JOIN locals l ON m.local_id = l.id JOIN users u ON u.id = r.author_id;
 
 CREATE VIEW meal_items AS SELECT m.id AS m_id, m.price AS m_price m.name AS m_name, l.id AS l_id, l.name AS l_name 
 	FROM meals m JOIN locals l ON m.local_id = l.id;
