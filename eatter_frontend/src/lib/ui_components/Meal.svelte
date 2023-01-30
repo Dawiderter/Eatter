430ed23503edcf71acf96d5a17a29f80f8f69f3b
Grubre <ogrodowczykjakub@gmail.com>
2023-01-30 22:52:20 +0100

    Added more info to meals

--- eatter_frontend/src/lib/ui_components/Meal.svelte
+++ eatter_frontend/src/lib/ui_components/Meal.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
-    export let meal_item : {m_id: number, m_price: number, m_name: string, l_id: number, l_name: number, m_r_num: number, m_r_avg: number};
+    export let meal_item : {m_id: number, m_price: number, m_name: string, l_id: number, l_name: number, m_r_num: number, m_r_avg: number | null};
 
     export let selected = false;
+    
+    let stars = meal_item.m_r_avg == null ? "?" : "★".repeat(meal_item.m_r_avg) + (meal_item.m_r_avg % 1 >= 0.5 ? "⯨" : "");
 
 </script>
 
@@ -12,13 +14,13 @@
         </a>
     {:else}
         <a class = "text-xl font-bold" href="/local{meal_item.l_id}/meal{meal_item.m_id}">
-            <h1>{meal_item.m_name}</h1>
+            <h1>{meal_item.m_name}({meal_item.m_r_num})</h1>
         </a>
     {/if}
     <h2 class = "font-raleway">{meal_item.m_price} zł
     <a href="/local{meal_item.l_id}"> &emsp; | &emsp;
         {meal_item.l_name}
-    </a> </h2>
+    </a>  &emsp; | &emsp; avg: {stars} </h2>
 
 </article>
 
--- eatter_server/mockdata.sql
+++ eatter_server/mockdata.sql
@@ -1,77 +1,20 @@
-CALL addUser('Flora@gmail.com', 'Flora', 'Flora123');
-CALL addUser('Peggy@gmail.com', 'Peggy', 'Peggy123');
-CALL addUser('Wilbert@gmail.com', 'Wilbert', 'Wilbert123');
-CALL addUser('Jesus@gmail.com', 'Jesus', 'Jesus123');
-CALL addUser('Toby@gmail.com', 'Toby', 'Toby123');
-CALL addUser('Clark@gmail.com', 'Clark', 'Clark123');
-CALL addUser('Winifred@gmail.com', 'Winifred', 'Winifred123');
-CALL addUser('Bryan@gmail.com', 'Bryan', 'Bryan123');
-CALL addUser('Patty@gmail.com', 'Patty', 'Patty123');
-CALL addUser('Kurt@gmail.com', 'Kurt', 'Kurt123');
-CALL addUser('Amber@gmail.com', 'Amber', 'Amber123');
-CALL addUser('Laurie@gmail.com', 'Laurie', 'Laurie123');
-CALL addUser('Mario@gmail.com', 'Mario', 'Mario123');
-CALL addUser('Candice@gmail.com', 'Candice', 'Candice123');
-CALL addUser('Ricky@gmail.com', 'Ricky', 'Ricky123');
-CALL addUser('Fredrick@gmail.com', 'Fredrick', 'Fredrick123');
-CALL addUser('Gregory@gmail.com', 'Gregory', 'Gregory123');
-CALL addUser('Robin@gmail.com', 'Robin', 'Robin123');
-CALL addUser('Samuel@gmail.com', 'Samuel', 'Samuel123');
-CALL addUser('Francis@gmail.com', 'Francis', 'Francis123');
-CALL addUser('Enrique@gmail.com', 'Enrique', 'Enrique123');
-CALL addUser('Cameron@gmail.com', 'Cameron', 'Cameron123');
-CALL addUser('Ralph@gmail.com', 'Ralph', 'Ralph123');
-CALL addUser('Elias@gmail.com', 'Elias', 'Elias123');
-CALL addUser('Peter@gmail.com', 'Peter', 'Peter123');
-CALL addUser('Bennie@gmail.com', 'Bennie', 'Bennie123');
-CALL addUser('Randal@gmail.com', 'Randal', 'Randal123');
-CALL addUser('Tabitha@gmail.com', 'Tabitha', 'Tabitha123');
-CALL addUser('Marjorie@gmail.com', 'Marjorie', 'Marjorie123');
-CALL addUser('Katrina@gmail.com', 'Katrina', 'Katrina123');
-CALL addUser('Jennifer@gmail.com', 'Jennifer', 'Jennifer123');
-CALL addUser('Mildred@gmail.com', 'Mildred', 'Mildred123');
-CALL addUser('Tammy@gmail.com', 'Tammy', 'Tammy123');
-CALL addUser('Pat@gmail.com', 'Pat', 'Pat123');
-CALL addUser('Edna@gmail.com', 'Edna', 'Edna123');
-CALL addUser('Darrin@gmail.com', 'Darrin', 'Darrin123');
-CALL addUser('Mona@gmail.com', 'Mona', 'Mona123');
-CALL addUser('Joanne@gmail.com', 'Joanne', 'Joanne123');
-CALL addUser('Willie@gmail.com', 'Willie', 'Willie123');
-CALL addUser('Lisa@gmail.com', 'Lisa', 'Lisa123');
-CALL addUser('Lance@gmail.com', 'Lance', 'Lance123');
-CALL addUser('Linda@gmail.com', 'Linda', 'Linda123');
-CALL addUser('Wade@gmail.com', 'Wade', 'Wade123');
-CALL addUser('Adrienne@gmail.com', 'Adrienne', 'Adrienne123');
-CALL addUser('Douglas@gmail.com', 'Douglas', 'Douglas123');
-CALL addUser('Greg@gmail.com', 'Greg', 'Greg123');
-CALL addUser('Melba@gmail.com', 'Melba', 'Melba123');
-CALL addUser('Roland@gmail.com', 'Roland', 'Roland123');
-CALL addUser('Gary@gmail.com', 'Gary', 'Gary123');
-CALL addUser('Kristen@gmail.com', 'Kristen', 'Kristen123');
+INSERT INTO companies(name) VALUES ("MCDonalds"), ("KFC"), ("Burger King");
 
+INSERT INTO locals(name, phone_num, contact_email, address, company_id) VALUES
+('MCD Plac Grunwaldzki', '623243932', 'placmcd@gmail.com', 'plac grunwaldzki 5', 1),
+('KFC Swidnicka', '590462442', 'kfcswidnicka@gmail.com', 'swidnicka 15', 2),
+('Burger King Rynek', '199887164', 'burgerrynek@gmail.com', 'rynek 7', 3);
+ 
+INSERT INTO meals(price, name, local_id) VALUES
+(25, 'Big Mac', 1),
+(30, 'Grander', 2),
+(22, 'Whooper', 3),
+(23, 'McChicken', 1),
+(10.5, 'Hamburger', 2),
+(5.75, 'Shake', 3);
 
+INSERT INTO tags(name) VALUES ('burger'), ('big'), ('shake');
 
-CALL addCompany('mcdonalds@gmail.com', 'ronald', 'ronaldmcd123', 'MCDonalds');
-CALL addCompany('kfc@gmail.com', 'colonel', 'colonelsanders1925', 'KFC');
-CALL addCompany('burgerking@gmail.com', 'burgerking', 'whooper12345', 'Burger King');
+INSERT INTO meals_tags(meal_id, tag_id) VALUES (1,1), (2,1), (3,1), (4,1), (5,1), (1,2), (6,3), (6,2);
 
-
-CALL addLocal(52, 'MCD Plac Grunwaldzki', '623243932', 'placmcd@gmail.com', 'plac grunwaldzki 5');
-CALL addLocal(53, 'KFC Swidnicka', '590462442', 'kfcswidnicka@gmail.com', 'swidnicka 15');
-CALL addLocal(54, 'Burger King Rynek', '199887164', 'burgerrynek@gmail.com', 'rynek 7');
-
-
-CALL addMeal(25, 'Big Mac', 1);
-CALL addMeal(30, 'Grander', 2);
-CALL addMeal(22, 'Whooper', 3);
-
-
-CALL addReview("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet dignissim metus, eget sollicitudin nunc cursus et. Suspendisse potenti. Donec ac interdum elit. Ut ac faucibus nisl. Nam ex dolor, porttitor sit amet commodo finibus, commodo tempor arcu. Donec euismod, leo id consequat libero.", 5, 1, 2);
-CALL addReview("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet dignissim metus, eget sollicitudin nunc cursus et. Suspendisse potenti. Donec ac interdum elit. Ut ac faucibus nisl. Nam ex dolor, porttitor sit amet commodo finibus, commodo tempor arcu. Donec euismod, leo id consequat libero.", 4, 2, 24);
-CALL addReview("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet dignissim metus, eget sollicitudin nunc cursus et. Suspendisse potenti. Donec ac interdum elit. Ut ac faucibus nisl. Nam ex dolor, porttitor sit amet commodo finibus, commodo tempor arcu. Donec euismod, leo id consequat libero.", 3, 3, 14);
-CALL addReview("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet dignissim metus, eget sollicitudin nunc cursus et. Suspendisse potenti. Donec ac interdum elit. Ut ac faucibus nisl. Nam ex dolor, porttitor sit amet commodo finibus, commodo tempor arcu. Donec euismod, leo id consequat libero.", 4, 2, 12);
-CALL addReview("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet dignissim metus, eget sollicitudin nunc cursus et. Suspendisse potenti. Donec ac interdum elit. Ut ac faucibus nisl. Nam ex dolor, porttitor sit amet commodo finibus, commodo tempor arcu. Donec euismod, leo id consequat libero.", 5, 1, 8);
-
-CALL addComment("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet dignissim metus, eget sollicitudin nunc cursus et. Suspendisse potenti.", 1, 12);
-CALL addComment("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet dignissim metus, eget sollicitudin nunc cursus et. Suspendisse potenti.", 2, 34);
-CALL addComment("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet dignissim metus, eget sollicitudin nunc cursus et. Suspendisse potenti.", 3, 40);
+UPDATE companies SET user_id = 1 WHERE id = 1;
