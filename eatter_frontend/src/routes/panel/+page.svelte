ca3cc3fdff07729a13c48322c182ecfa5c2c6743
Grubre <ogrodowczykjakub@gmail.com>
2023-01-30 23:22:06 +0100

    Panel update

--- eatter_frontend/src/routes/panel/+page.svelte
+++ eatter_frontend/src/routes/panel/+page.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
     import { page } from '$app/stores';
+	import Local from '$lib/ui_components/Local.svelte';
 
     import type { PageData } from "./$types";
 
     export let data : PageData;
 </script>
 
+<div class = "flex flex-col items-center justify-center">
+    <h1 class = "mt-[10px] text-2xl font-raleway">Manage your company</h1>
+    {#each data.locals as local}
+        <Local l = {local}></Local>
+    {/each}
+</div>
