43c88fb946cf4b53d373aabf11bfa5d8740cbf54
Grubre <ogrodowczykjakub@gmail.com>
2023-01-31 00:01:06 +0100

    Added frontend for adding items

--- eatter_frontend/src/routes/local[lid]/+page.svelte
+++ eatter_frontend/src/routes/local[lid]/+page.svelte
@@ -1,14 +1,33 @@
 <script lang="ts">
 	import MealCard from "$lib/ui_components/Meal.svelte";
 
-    export let data : import("./$types").PageServerData;
+    export let data : import("./$types").PageData;
 
 </script>
 
 <div class = "">
+	
 	{#each data.items as item}
-		<MealCard 
-			meal_item = {item} 
-		/>
+	<div class = "flex flex-row w-screen  items-center justify-center">
+		{#if data.auth != null && data.auth.company_id != null}
+			<button class="p-5"  on:click={() => {
+					
+			}}>
+				<span class="close">+</span>
+			</button>
+		{/if}
+			<MealCard 
+				meal_item = {item} 
+			/>
+		</div>
 	{/each}
 </div>
+
+<style>
+    .close {
+  font-size: 25px;
+  font-weight: 200;
+  display: inline-block;
+  transform: rotate(45deg);
+}
+</style>
--- eatter_frontend/src/routes/panel/+page.svelte
+++ eatter_frontend/src/routes/panel/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { page } from '$app/stores';
+	import ItemForm from "$lib/ui_components/ItemForm.svelte";
 	import Local from '$lib/ui_components/Local.svelte';
 
     import type { PageData } from "./$types";
@@ -8,8 +9,29 @@
 </script>
 
 <div class = "flex flex-col items-center justify-center">
-    <h1 class = "mt-[10px] text-2xl font-raleway">Manage your company</h1>
+    <h1 class = "mt-[10px] p-10 text-6xl font-raleway">Manage your company</h1>
+    <h2 class = "pt-5 text-2xl font-raleway">Your locals:</h2>
+    <ItemForm action="" fields={[{name: "name", type: "text"},
+    {name: "phone number", type: "text"},
+    {name: "contact email", type: "text"},
+    {name: "address", type: "text"}]}></ItemForm>
     {#each data.locals as local}
-        <Local l = {local}></Local>
+    <div class = "flex flex-row w-screen items-center justify-center p-5">
+        <button class="p-5" on:click={() => {
+            
+        }}>
+            <span class="close">+</span>
+        </button>
+            <Local l = {local}></Local> 
+    </div>
     {/each}
 </div>
+
+<style>
+    .close {
+  font-size: 55px;
+  font-weight: 200;
+  display: inline-block;
+  transform: rotate(45deg);
+}
+</style>
