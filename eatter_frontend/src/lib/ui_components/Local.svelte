59b71d5ec36ff006995fdd9878e3f0b2968c93be
Grubre <ogrodowczykjakub@gmail.com>
2023-01-30 18:50:34 +0100

    Changed local

--- eatter_frontend/src/lib/ui_components/Local.svelte
+++ eatter_frontend/src/lib/ui_components/Local.svelte
@@ -2,14 +2,15 @@
     export let l : {l_id : number, l_name : string, l_phone_num : string, l_contact_email : string, l_address : string, c_id : number, c_name : string};
 </script>
 
-<div class = "rounded-lg w-3/12 bg-gray-200 border-r-2 shadow-lg p-8 ">
-    <h2 class = "font-jetbrains text-xl">{l.c_name}</h2>
-    <a href="/local{l.l_id}">
-        <h3 class = "font-raleway text-xs tab-6">{l.l_name}</h3>
-    </a>
-    <ul class = "p-3 text-l font-sans">
-        <li>{l.l_phone_num}</li>
-        <li>{l.l_contact_email}</li>
-        <li>{l.l_address}</li>
-    </ul>
+<div class = "rounded-3xl w-3/12 bg-gray-200 border-r-2 shadow-lg p-8 ">
+    <h2 class = "p-5 text-4xl font-jetbrains font-bold">{l.c_name}
+        <a href="/local{l.l_id}">
+            <p class = "font-raleway text-xs tab-6">@{l.l_name}</p>
+        </a>
+    </h2>
+    <p>{l.l_phone_num} | {l.l_contact_email} | {l.l_address}</p>
+    <!-- <div class = "rounded-3xl w-3/12 bg-gray-200 border-r-2 shadow-lg p-8">
+        <h1 class = "p-5 text-4xl font-jetbrains font-bold">{data.local.l_name}</h1>
+        <h2 class = "pt-5 font-raleway">{header}</h2>
+    </div> -->
 </div>
